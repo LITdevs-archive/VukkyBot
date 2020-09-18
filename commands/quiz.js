0677d2b0075743c1c2e97699663decdacfc52127
Vukky <46850780+VukAnd@users.noreply.github.com>
2020-09-18 16:46:34 +0200

    It wouldn't be VukkyBot without the shitty code

--- commands/quiz.js
+++ commands/quiz.js
@@ -14,10 +14,33 @@ module.exports = {
             message.channel.awaitMessages(filter, { max: 1, time: 30000, errors: ['time'] })
                 .then(collected => {
                     message.channel.send(`${collected.first().author} got the correct answer!`);
+                    wannaPlayAgain(message, args)
                 })
                 .catch(collected => {
                     message.channel.send('Looks like nobody got the answer this time.');
+                    wannaPlayAgain(message, args)
                 });
         });
+
+        function wannaPlayAgain(message, args) {
+            message.channel.send("Wanna play again?").then((confirmMessage) => {
+                confirmMessage.react('👍').then(() => confirmMessage.react('👎'));
+                const filter = (reaction, user) => {
+                    return ['👍', '👎'].includes(reaction.emoji.name) && user.bot === false
+                };
+                confirmMessage.awaitReactions(filter, { max: 1, time: 15000, errors: ['time'] })
+                    .then(collected => {
+                        const reaction = collected.first();
+                        if (reaction.emoji.name === '👍') {
+                            message.client.commands.get("quiz").execute(message, args)
+                        } else {
+                            confirmMessage.channel.send("Okay! There's always next time, I guess.");
+                        }
+                    })
+                    .catch(collected => {
+                        confirmMessage.channel.send("Hm! I don't think anyone wants to play again...");
+                });
+            });
+        }
 	},
 };
