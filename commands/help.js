5dda8b8bb221e9f0c2e13232ca092a3f9dd8e3d6
Vukky <46850780+VukAnd@users.noreply.github.com>
2020-09-22 17:14:33 +0200

    add cooldown to help

--- commands/help.js
+++ commands/help.js
@@ -38,7 +38,8 @@ module.exports = {
 
 	    if (command.aliases) data.push(`**Aliases:** ${command.aliases.join(', ')}`);
 	    if (command.description) data.push(`**Description:** ${command.description}`);
-	    if (command.usage) data.push(`**Usage:** ${prefix}${command.name} ${command.usage}`);
+		if (command.usage) data.push(`**Usage:** ${prefix}${command.name} ${command.usage}`);
+		if (command.cooldown) data.push(`**Cooldown:** ${command.cooldown} second(s)`);
 
 	    message.channel.send(data, { split: true });
 	},
