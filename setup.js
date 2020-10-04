240c1120e170b928802db56804fc9fd0127c6900
Vukky <46850780+VukAnd@users.noreply.github.com>
2020-10-04 16:10:51 +0200

    setup tool improvements

--- setup.js
+++ setup.js
@@ -15,11 +15,40 @@ var questions = [
         type: 'input',
         name: 'token',
         message: 'What\'s your bot token?'
-    }
+    },
+    {
+        type: 'confirm',
+        name: 'launch',
+        message: 'Start VukkyBot after setup is complete?'
+    },
 ];
   
 inquirer.prompt(questions).then((answers) => {
-    fs.writeFile('.env', `BOT_TOKEN=${answers.token}\nPREFIX=${answers.prefix}`, function (err) {
-        if (err) return console.log(err);
-    });
+    const ora = require('ora');
+    const spinner = ora('Saving to .env').start();
+    try {
+        fs.writeFile('.env', `BOT_TOKEN=${answers.token}\nPREFIX=${answers.prefix}`, function (err) {
+            if (err) {
+                spinner.fail("Saving failed")
+                console.log(err)
+            }
+        });
+        spinner.succeed("Saved to .env")
+    } catch (err) {
+        spinner.fail("Saving to .env failed")
+        console.log(err)
+    }
+    if (answers.launch == true) {
+        const spinner = ora('Starting VukkyBot').start();
+        try {
+            const npm = require('npm');
+            npm.load(() => {
+                npm.run('start');
+            });
+            spinner.succeed("VukkyBot should start now")
+        } catch (err) {
+            spinner.fail("Couldn't start VukkyBot")
+            console.log(err)
+        }
+    }
 });
