f4d7d3fd15ac7c045da0972b572659e4ed6fb4dd
Vukky <46850780+VukAnd@users.noreply.github.com>
2020-10-06 17:17:09 +0200

    add kick and ban

---
+++ commands/kick.js
@@ -0,0 +1,26 @@
+const embeds = require("../embeds.js");
+
+module.exports = {
+	name: 'kick',
+	description: 'Kick someone',
+    dcPermissions: ['EMBED_LINKS', 'KICK_MEMBERS'],
+    guildOnly: true,
+    args: true,
+    usage: "<@user>",
+	execute(message, args) {
+        var mentionedUser = message.guild.member(message.mentions.users.first())
+        var kickReason = args.slice(1).join(' ') || "no reason specified";
+        if (!message.member.hasPermission("KICK_MEMBERS")) return message.channel.send("You need the Kick Members permission to do that!")
+        if (!mentionedUser) return message.channel.send("You need to provide a valid user.")
+        if(mentionedUser.id === message.author.id) return message.channel.send('You can\'t kick yourself! That would be silly.');
+        if(mentionedUser.id === message.client.user.id) return message.channel.send(':(');
+
+        var authorHighestRole = message.member.roles.highest.position;
+        var mentionHighestRole = mentionedUser.roles.highest.position;
+        if(mentionHighestRole >= authorHighestRole) return message.channel.send('You can\'t kick members with an equal or higher position than you.');
+        if(!mentionedUser.kickable) return message.channel.send('I can\'t kick this user.');
+
+        mentionedUser.kick(`Done by ${message.author.tag} - ${kickReason}`)
+            .then(user => message.channel.send(embeds.successEmbed(`Kicked <@${user.id}> (${user.id}) from ${message.guild.name}, with the reason ${kickReason}.`)))
+	},
+};
