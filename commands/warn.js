c9d0c4811c85668951baf27038c01bd140706bd3
viluzi <47392011+viluzi@users.noreply.github.com>
2020-10-11 14:30:59 +0300

    delwarn and warns, also fixed bug of server timing out

--- commands/warn.js
+++ commands/warn.js
@@ -3,17 +3,6 @@ require('dotenv').config();
 var mysql = require('mysql');
 const { errorEmbed, successEmbed } = require("../embeds.js");
 
-var con = mysql.createConnection({
-  host: process.env.SQL_HOST,
-  user: process.env.SQL_USER,
-  password: process.env.SQL_PASS,
-  database: process.env.SQL_DB
-});
-
-con.connect(function(err) {
-    if (err) throw err;
-    console.log("Connected to the database!");
-});
 
 module.exports = {
     name: 'warn',
@@ -21,19 +10,31 @@ module.exports = {
     dcPermissions: ['EMBED_LINKS', 'MANAGE_MESSAGES'],
     usage: "<@user> <reason>",
     execute(message, args) {
-        console.log(config.commands.warn.mysql)
         if (config.commands.warn.mysql) {
         let mentionedUser = message.guild.member(message.mentions.users.first())
         if (!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send(errorEmbed("You need the Manage Messages permission to do that!"))
             if (args.slice(1).join(' ').length >= 1) {
+                var con = mysql.createConnection({
+                    host: process.env.SQL_HOST,
+                    user: process.env.SQL_USER,
+                    password: process.env.SQL_PASS,
+                    database: process.env.SQL_DB
+                  });
+                  
+                  con.connect(function(err) {
+                      if (err) console.log(err);
+                  });
+                  
             let sql = "INSERT INTO warnings (username, uid, reason) VALUES ('" + mentionedUser.user.username + "', " + mentionedUser.id + ", '" + args.slice(1).join(' ') + "')";
             con.query(sql, function (err, result) {
             if (err)  {
                 message.channel.send(errorEmbed("An error has occurred! See logs for more information."));
-                throw err;
+                console.log(err);
+                con.end()
             } else {
                 message.channel.send(successEmbed(`Warning added to ${mentionedUser} for reason ${args.slice(1).join(' ')}!`));
                 console.log("1 warning added");
+                con.end()
             }
             });
         } else {
---
+++ commands/warns.js
@@ -0,0 +1,51 @@
+const config = require('../config.json');
+require('dotenv').config();
+var mysql = require('mysql');
+const { errorEmbed, successEmbed } = require("../embeds.js");
+
+
+module.exports = {
+    name: 'warns',
+    description: 'Make VukkyBot remove warnings from people!',
+    dcPermissions: ['EMBED_LINKS', 'MANAGE_MESSAGES'],
+    args: true,
+    usage: "<warning id>",
+    execute(message, args) {
+        let warnsId    
+        if (isNaN(args.slice(0).join(' '))) {
+            let mentionedUser = message.guild.member(message.mentions.users.first())
+            warnsId = mentionedUser.id
+        } else {
+            warnsId = args.slice(0).join(' ')
+        }
+        var con = mysql.createConnection({
+            host: process.env.SQL_HOST,
+            user: process.env.SQL_USER,
+            password: process.env.SQL_PASS,
+            database: process.env.SQL_DB
+          });
+          
+          con.connect(function(err) {
+              if (err) console.log(err);
+          });
+          
+            let sql = `SELECT * FROM warnings WHERE uid = ${warnsId}`;
+            con.query(sql, function (err, result) {
+            if (err)  {
+                message.channel.send(errorEmbed("An error has occurred! See logs for more information."));
+                console.log(err);
+                con.end()
+            } else {
+                if (result.length == 0) { con.end(); return message.channel.send(successEmbed("This person has no warnings!")); }
+                let finalMessage = `Warnings for ${result[0].username}: \n`;
+                
+                for (let i = 0; i < result.length; i++) {
+                    finalMessage = finalMessage.concat("reason: ", result[i].reason + " id: ", result[i].id + "\n")
+                }
+                message.channel.send(successEmbed(finalMessage))
+                con.end()
+            }
+            });
+        
+    },
+};
