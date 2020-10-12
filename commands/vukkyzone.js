ea504d66fb57b4ab467f4176520c3fece952615e
Vukky <46850780+VukAnd@users.noreply.github.com>
2020-10-12 22:01:31 +0200

    add vukkyzone

---
+++ commands/vukkyzone.js
@@ -0,0 +1,23 @@
+const embeds = require("../embeds.js");
+
+module.exports = {
+	name: 'vukkyzone',
+	description: 'Start a Vukky Zone.',
+	dcPermissions: ['EMBED_LINKS', 'ADD_REACTIONS', 'MANAGE_MESSAGES'],
+	execute(message, args) {
+        message.delete()
+        message.channel.send(`**Welcome to the Vukky Zone!** (started by <@${message.author.id}>)\nReact with ✨ to join, or 💥 to destroy.`).then(vukkyzone => {
+            vukkyzone.react('✨').then(() => vukkyzone.react('💥'));
+            const filter = (reaction, user) => {
+                return ['💥', '✨'].includes(reaction.emoji.name) && user.bot == false
+            };
+            
+            vukkyzone.awaitReactions(filter)
+                .then(collected => {
+                    const reaction = collected.first();
+                    vukkyzone.edit(`💥 The Vukky Zone was destroyed!\n${vukkyzone.reactions.cache.get('✨').count - 1} user(s) entered before the Vukky Zone was destroyed.`)
+                    vukkyzone.reactions.removeAll()
+                })
+        })
+	},
+};
