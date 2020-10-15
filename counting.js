051f668643695a3bfb6048aae2978116952b9cfb
Vukky <46850780+VukAnd@users.noreply.github.com>
2020-10-15 20:36:00 +0200

    chalk makeover for counting.js

--- counting.js
+++ counting.js
@@ -1,10 +1,16 @@
+var chalk = require("chalk")
 var mysql = require("mysql")
 var config = require("./config.json")
 require('dotenv').config();
 let sql
+
+const error = chalk.bold.red;
+const success = chalk.green;
+const info = chalk.blue;
+
 module.exports = {
     start: function() {
-        if(!config.misc.mysql) return;
+        if(!config.misc.mysql) return console.log(`[counting] ${error("MySQL is not enabled")}`)
         let con = mysql.createConnection({
             host: process.env.SQL_HOST,
             user: process.env.SQL_USER,
@@ -14,17 +20,17 @@ module.exports = {
 
         con.connect(function(err) {
             if (err) {
-                console.log("[counting] Failed to connect to the database")
+                console.log(`[counting] ${error("Failed to connect to the database")}`)
                 console.log(err)
             } else {
-                console.log("[counting] Connected to the database")
+                console.log(`[counting] ${success("Connected to the database")}`)
                 sql = "CREATE TABLE counting (serverid VARCHAR(255), number VARCHAR(255), lastcounter VARCHAR(255), id INT AUTO_INCREMENT PRIMARY KEY)";
                 con.query(sql, function (err, result) {
                 if (err) {
-                    console.log("[counting] Table creation failed (Probably already exists)")
+                    console.log(`[counting] ${error("Table creation failed")} (Probably already exists)`)
                     con.end()
                 } else {
-                    console.log("[counting] Table created")
+                    console.log(`[counting] ${success("Table created")}`)
                     con.end()
                 }
                 });
@@ -33,7 +39,7 @@ module.exports = {
 
     },
     countCheck: function (message) {
-        if(!config.misc.mysql) return;
+        if(!config.misc.mysql) return console.log(`[counting] ${error("MySQL is not enabled")}`)
         let con = mysql.createConnection({
             host: process.env.SQL_HOST,
             user: process.env.SQL_USER,
@@ -44,13 +50,13 @@ module.exports = {
        sql = "SELECT * FROM counting WHERE serverid = " + message.guild.id;
        con.query(sql, function (err, result) {
         if (err) {
-            console.log("[counting] something went wrong fetching current number. Maybe it doesnt exist?")
+            console.log(`[counting] ${error("Something went wrong while fetching the current number. Maybe it doesn't exist?")}`)
             con.end()
         } else {
             let currentNumber
             let lastCounter
             if (result.length <= 0) {
-                console.log("[counting] Creating row for server...")
+                console.log(`[counting] ${info("Creating row for server...")}`)
                 let con = mysql.createConnection({
                     host: process.env.SQL_HOST,
                     user: process.env.SQL_USER,
@@ -60,13 +66,13 @@ module.exports = {
 
                 con.connect(function(err) {
                     if (err) {
-                        console.log("[counting] Failed to connect to the database")
+                        console.log(`[counting] ${error("Failed to connect to the database")}`)
                         console.log(err)
                     } else {
                         sql = `INSERT INTO counting(serverid, number) VALUES (${message.guild.id}, 0)`;
                         con.query(sql, function (err, result) {
                         if (err) {
-                            console.log("[counting] Failed to create row")
+                            console.log(`[counting] ${error("Failed to create row")}`)
                             con.end()
                         } else {
                             con.end()
@@ -94,12 +100,12 @@ module.exports = {
                         sql = `UPDATE counting set number = ${message.content.split(' ')[0]}, lastcounter = ${message.author.id} WHERE serverid = ${message.guild.id}`
                         con.connect(function(err) {
                             if (err) {
-                                console.log("[counting] Something went wrong")
+                                console.log(`[counting] ${error("Something went wrong")}`)
                                 console.log(err)
                             } else {
                                 con.query(sql, function (err, result) {
                                 if (err) {
-                                    console.log("[counting] something went wrong")
+                                    console.log(`[counting] ${error("Something went wrong")}`)
                                     con.end()
                                 } else {
                                     con.end()
@@ -119,12 +125,12 @@ module.exports = {
                         sql = `UPDATE counting set number = 0, lastcounter = 0 WHERE serverid = ${message.guild.id}`
                         con.connect(function(err) {
                             if (err) {
-                                console.log("[counting] Something went wrong")
+                                console.log(`[counting] ${error("Something went wrong")}`)
                                 console.log(err)
                             } else {
                                 con.query(sql, function (err, result) {
                                 if (err) {
-                                    console.log("[counting] something went wrong")
+                                    console.log(`[counting] ${error("Something went wrong")}`)
                                     con.end()
                                 } else {
                                     con.end()
@@ -145,12 +151,12 @@ module.exports = {
                     sql = `UPDATE counting set number = 0, lastcounter = 0 WHERE serverid = ${message.guild.id}`
                     con.connect(function(err) {
                         if (err) {
-                            console.log("[counting] Something went wrong")
+                            console.log(`[counting] ${error("Something went wrong")}`)
                             console.log(err)
                         } else {
                             con.query(sql, function (err, result) {
                             if (err) {
-                                console.log("[counting] something went wrong")
+                                console.log(`[counting] ${error("Something went wrong")}`)
                                 con.end()
                             } else {
                                 con.end()
