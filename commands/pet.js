ab954aae8e448869770e67bd130e490d185d9670
Fvn556 <Fvn556@users.noreply.github.com>
2020-11-02 13:22:08 -0500

    Made bot favor pets to me and when you pet someone other than me it questions you

--- commands/pet.js
+++ commands/pet.js
@@ -12,7 +12,8 @@ module.exports = {
 		} else {
 			if (message.mentions.users.first().id === message.author.id) return message.channel.send("You can't pet yourself! That would be silly.");
 			if (message.mentions.users.first().id === message.client.user.id) return message.channel.send("Aww, thanks! ♥");
-			message.channel.send(`<@${message.author.id}> pets <@${message.mentions.users.first().id}>!`);
+			if (message.mentions.users.first().id === "468242178880176129") return message.channel.send(`<@${message.author.id}> pets <@${message.mentions.users.first().id}>! Ty for petting Fvn!!`);
+			message.channel.send(`<@${message.author.id}> pets <@${message.mentions.users.first().id}>! But why are you petting someone other then Fvn...`);
 		}
 	},
 };
