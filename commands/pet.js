e02cea46a50691d7372048122c0929636e3c2c23
Fvn556 <Fvn556@users.noreply.github.com>
2020-11-03 10:28:25 -0500

    add stuff

--- commands/pet.js
+++ commands/pet.js
@@ -1,3 +1,4 @@
+const config = require("../config.json");
 const embeds = require("../embeds.js");
 
 module.exports = {
@@ -10,10 +11,13 @@ module.exports = {
 		if(!message.mentions.users.size) {
 			message.channel.send(embeds.errorEmbed("You need to ping someone to use this command :("));
 		} else {
-			if (message.mentions.users.first().id === message.author.id) return message.channel.send("You can't pet yourself! That would be silly.");
-			if (message.mentions.users.first().id === message.client.user.id) return message.channel.send("Aww, thanks! ♥");
+			if (message.mentions.users.first().id === message.author.id && message.author.id != config.misc.owner) return message.channel.send("Petting yourself is a true sign of loneliness. I feel bad for you.");
+			if (message.mentions.users.first().id === message.author.id && message.author.id === config.misc.owner) return message.channel.send("Fvn, don't.");
+			if (message.mentions.users.first().id === message.client.user.id && message.author.id != config.misc.owner) return message.channel.send("Thanks, but you should've pet Fvn instead...");
+			if (message.mentions.users.first().id === message.client.user.id && message.author.id === config.misc.owner) return message.channel.send("Fvn just pet me!! Yay!!!");
 			if (message.mentions.users.first().id === "468242178880176129") return message.channel.send(`<@${message.author.id}> pets <@${message.mentions.users.first().id}>! Ty for petting Fvn!!`);
-			message.channel.send(`<@${message.author.id}> pets <@${message.mentions.users.first().id}>! But why are you petting someone other then Fvn...`);
+			if (message.author.id === config.misc.owner) return message.channel.send(`Woah!! Fvn just pet <@${message.mentions.users.first().id}>! Luckyyyyy`);
+			message.channel.send(`<@${message.author.id}> pets <@${message.mentions.users.first().id}>! But why are you petting someone other than Fvn...`);
 		}
 	},
 };
