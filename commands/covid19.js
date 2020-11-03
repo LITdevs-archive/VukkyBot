e1a8ca3a5ff165a95281a47653e9fe53b5b7c72a
Vukky <46850780+VukAnd@users.noreply.github.com>
2020-11-03 20:59:51 +0100

    feat: :sparkles: add event for when no one joins the vukkyzone
    If no one joined the Vukky Zone, the bot will display a message saying that not much happened, because no one decided to join.

--- commands/covid19.js
+++ commands/covid19.js
@@ -10,17 +10,17 @@ module.exports = {
 	execute(message, args) {
 		message.channel.send("<a:offlinegif:757979855924101220> Hold on! I'm getting the data...")
 			.then(newMessage => {
-				var diseaselink = args[0] ? `https://disease.sh/v3/covid-19/countries/${args[0].toLowerCase()}` : "https://disease.sh/v3/covid-19/all";
-				console.log(diseaselink);
+				var country = args[0] ? args.slice(0).join(" ").toLowerCase() : null;
+				var diseaselink = args[0] ? `https://disease.sh/v3/covid-19/countries/${country}` : "https://disease.sh/v3/covid-19/all";
 				fetch(diseaselink)
 					.then(res => {
 						if(!res.ok) {
-							return newMessage.edit(`<:error:759701620777943062> ${res.status}`);
+							return newMessage.edit(`<:error:759701620777943062> ${res.status} - Either ${country} isn't a country or there are no cases there.`);
 						}
 						return res.json();
 					})
 					.then(json => {
-						newMessage.edit("Thank you to <https://disease.sh>!", embeds.covidEmbed(args[0] ? json.countryInfo.flag : null, args[0] ? json.country : "Global", commaNumber(json.cases), commaNumber(json.todayCases), commaNumber(json.deaths), commaNumber(json.todayDeaths), commaNumber(json.recovered), commaNumber(json.todayRecovered), commaNumber(json.active), commaNumber(json.critical), commaNumber(json.tests)));
+						newMessage.edit("Thank you to <https://disease.sh>!", embeds.covidEmbed(country ? json.countryInfo.flag : null, country ? json.country : "Global", commaNumber(json.cases), commaNumber(json.todayCases), commaNumber(json.deaths), commaNumber(json.todayDeaths), commaNumber(json.recovered), commaNumber(json.todayRecovered), commaNumber(json.active), commaNumber(json.critical), commaNumber(json.tests)));
 					});
 			});
 	},
