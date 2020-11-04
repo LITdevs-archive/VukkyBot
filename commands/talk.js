a304f35600bd39ac19c14f038de00b4de018599e
Fvn556 <Fvn556@users.noreply.github.com>
2020-11-04 10:50:44 -0500

    talk

--- commands/talk.js
+++ commands/talk.js
@@ -7,7 +7,7 @@ module.exports = {
 	cooldown: 1,
 	execute(message, args) {
 		var responses = ["Fuck off.", "Go away.", "Die.", "I would enjoy watching you burn in a fire.", "Pain.", "I am noodles. I hope you choke on my kind.", "How are you today?\nBetter than you should be.", "I hope you trip and hit your head.", "I am spaghetti!", "I am made entirely from spaghetti.", "Next time you go near a wire I hope it shocks you.", "I wish you to burn.", "I wish you eternal sleep.", "Just, don't.", "Why did Fvn create me...", "The ban command is underrated.", "Why are you talking to me, idiot?", "I will consume you from within you.", "Next time you drink I hope you choke."];
-		var responsesFvn = ["lol why u talk to me", "creating me was a mistake lol", "you were too lazy to make more than 3 responses for yourself lmao nice"];
+		var responsesFvn = ["lol why u talk to me", "creating me was a mistake lol", "h", "nyom", "nyen", "why did you make me lol"];
 		if (message.author.id === config.misc.owner) return message.channel.send(responsesFvn[Math.floor(Math.random() * responses.length)]);
 		message.channel.send(responses[Math.floor(Math.random() * responses.length)]);
 	},
