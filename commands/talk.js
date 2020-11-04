de06d5af216f8a4d00f3e79d92ec7022a8de6c23
Fvn556 <Fvn556@users.noreply.github.com>
2020-11-04 11:40:34 -0500

    trying to make the bot not abuse my children when they talk to it

--- commands/talk.js
+++ commands/talk.js
@@ -8,7 +8,9 @@ module.exports = {
 	execute(message, args) {
 		var responses = ["Fuck off.", "Go away.", "Die.", "I would enjoy watching you burn in a fire.", "Pain.", "I am noodles. I hope you choke on my kind.", "How are you today?\nBetter than you should be.", "I hope you trip and hit your head.", "I am spaghetti!", "I am made entirely from spaghetti.", "Next time you go near a wire I hope it shocks you.", "I wish you to burn.", "I wish you eternal sleep.", "Just, don't.", "Why did Fvn create me...", "The ban command is underrated.", "Why are you talking to me, idiot?", "I will consume you from within you.", "Next time you drink I hope you choke."];
 		var responsesFvn = ["lol why u talk to me", "creating me was a mistake lol", "h", "nyom", "nyen", "why did you make me lol"];
+		var responsesChild = ["hello, one of Fvn's children.", "*pat pat*", "h", "nyom", "nyen", "Fvn is cool, right?", ];
 		if (message.author.id === config.misc.owner) return message.channel.send(responsesFvn[Math.floor(Math.random() * responsesFvn.length)]);
+		if (message.author.id === config.misc.children) return message.channel.send(responsesChild[Math.floor(Math.random() * responsesChild.length)]);
 		message.channel.send(responses[Math.floor(Math.random() * responses.length)]);
 	},
 };
