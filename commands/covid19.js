fcf29bdcacd00cca7399f9fdc1a61fe0db59d75b
Vukky <46850780+VukAnd@users.noreply.github.com>
2021-01-08 14:50:52 +0100

    add emoji customization

--- commands/covid19.js
+++ commands/covid19.js
@@ -1,4 +1,5 @@
 const embeds = require("../utilities/embeds");
+const config = require("../config.json");
 const fetch = require("node-fetch");
 const commaNumber = require("comma-number");
 const { Util } = require("discord.js");
@@ -9,14 +10,14 @@ module.exports = {
 	botPermissions: ["EMBED_LINKS"],
 	aliases: ["covid", "covid-19"],
 	execute(message, args) {
-		message.channel.send("<a:offlinegif:757979855924101220> Hold on! I'm getting the data...")
+		message.channel.send(`${config.misc.emoji.loading} Hold on! I'm getting the data...`)
 			.then(newMessage => {
 				var country = args[0] ? Util.removeMentions(args.slice(0).join(" ").toLowerCase()) : null;
 				var diseaselink = args[0] ? `https://disease.sh/v3/covid-19/countries/${country}` : "https://disease.sh/v3/covid-19/all";
 				fetch(diseaselink)
 					.then(res => {
 						if(!res.ok) {
-							return newMessage.edit(`<:error:759701620777943062> ${res.status} - Either ${country} isn't a country or there are no cases there.`);
+							return newMessage.edit(`${config.misc.emoji.error} ${res.status} - Either ${country} isn't a country, there are no cases there, or something bad happened.`);
 						}
 						return res.json();
 					})
--- commands/crypto.js
+++ commands/crypto.js
@@ -1,5 +1,7 @@
 const embeds = require("../utilities/embeds");
 const fetch = require("node-fetch");
+const config = require("../config.json");
+
 
 module.exports = {
 	name: "crypto",
@@ -13,16 +15,16 @@ module.exports = {
 		function checkStatus(res, newMessage) {
 			checkStatusVar = 1;
 			if(res.status == 400) {
-				return newMessage.edit(`<:error:759701620777943062> You messed up your input. Are you sure ${args[0].toUpperCase()} is a valid coin? The server returned 400.`);
+				return newMessage.edit(`${config.misc.emoji.error} Are you sure ${args[0].toUpperCase()} is a valid coin?`);
 			} else if (res.status == 502) {
-				return newMessage.edit("<:error:759701620777943062> The server appears to be unavailable. Come back later. The server returned 502.");
+				return newMessage.edit(`${config.misc.emoji.error} The server appears to be unavailable. Come back later.`);
 			} else if (res.status == 429) {
-				return newMessage.edit("<:error:759701620777943062> VukkyBot has been ratelimited from accessing the API. Come back later. The server returned 429.");
+				return newMessage.edit(`${config.misc.emoji.error} VukkyBot has been ratelimited from accessing the API.`);
 			} else {
-				return newMessage.edit("<:error:759701620777943062> Something is broken!");
+				return newMessage.edit(`${config.misc.emoji.error} Something is broken!`);
 			}
 		}
-		message.channel.send("<a:offlinegif:757979855924101220> Hold on! I'm getting the data...")
+		message.channel.send(`${config.misc.emoji.loading} Hold on! I'm getting the data...`)
 			.then(newMessage => {
 				fetch(`http://api.shruc.ml/saladlog/price?coin=${args[0].toLowerCase()}`)
 					.then(res => {
@@ -36,7 +38,7 @@ module.exports = {
 							newMessage.edit("Thanks to SharkOfGod for the API!", embeds.cryptoEmbed(json.RAW.FROMSYMBOL, json.RAW.PRICE, json.DISPLAY.LASTUPDATE.toLowerCase(), json.RAW.CHANGE24HOUR.toFixed(2)));
 						} catch (error) {
 							console.log(error.message);
-							if(!checkStatusVar == 1) newMessage.edit(`<:error:759701620777943062> Some funny stuff happened while attempting to display the data. Try again later. \`${error.message}\``);
+							if(!checkStatusVar == 1) newMessage.edit(`${config.misc.emoji.error} Some funny stuff happened while attempting to display the data. Try again later. \`${error.message}\``);
 							return;
 						}
 					});
