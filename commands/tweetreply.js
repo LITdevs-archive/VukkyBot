f033de199bdceabc0d4d7963b7e2d88868bb404c
Vukky <46850780+VukAnd@users.noreply.github.com>
2021-01-11 18:05:08 +0100

    Update tweetreply.js

--- commands/tweetreply.js
+++ commands/tweetreply.js
@@ -1,6 +1,7 @@
 const embeds = require("../utilities/embeds");
 require("dotenv").config();
 var Twitter = require("twitter");
+const { Util } = require("discord.js");
 
 module.exports = {
 	name: "tweetreply",
@@ -15,29 +16,32 @@ module.exports = {
 		const filter = (reaction, user) => {
 			return ["⬆", "⬇"].includes(reaction.emoji.name) && user.id != message.author.id && user.bot == false;
 		};
-		message.awaitReactions(filter, { max: 1 })
-			.then(collected => {
-				message.reactions.removeAll();
-				const reaction = collected.first();
-				if(reaction.emoji.name == "⬆") {
-					var client = new Twitter({
-						consumer_key: process.env.TWITTER_KEY,
-						consumer_secret: process.env.TWITTER_SECRET,
-						access_token_key: process.env.TWITTER_ACCESS,
-						access_token_secret: process.env.TWITTER_ACCESS_SECRET
-					});
-					client.post("statuses/update", {status: args.slice(1).join(" "), in_reply_to_status_id: args[0], auto_populate_reply_metadata: true})
-						.then(function (tweet) {
-							message.react("✅");
-							message.reply(`your tweet was approved by a user! Here it is: https://twitter.com/i/status/${tweet.id_str}`);
-						})
-						.catch(function (error) {
-							throw error;
+		message.reply(`you are replying to ${Util.removeMentions(`https://twitter.com/i/status/${args[0]}`)}`).then(tweetreplyDisclaimer => {
+			message.awaitReactions(filter, { max: 1 })
+				.then(collected => {
+					message.reactions.removeAll();
+					tweetreplyDisclaimer.delete();
+					const reaction = collected.first();
+					if(reaction.emoji.name == "⬆") {
+						var client = new Twitter({
+							consumer_key: process.env.TWITTER_KEY,
+							consumer_secret: process.env.TWITTER_SECRET,
+							access_token_key: process.env.TWITTER_ACCESS,
+							access_token_secret: process.env.TWITTER_ACCESS_SECRET
 						});
-				} else if (reaction.emoji.name == "⬇") {
-					message.react("❌");
-					message.reply("your tweet was denied by a user.");
-				}
-			});
+						client.post("statuses/update", {status: args.slice(1).join(" "), in_reply_to_status_id: args[0], auto_populate_reply_metadata: true})
+							.then(function (tweet) {
+								message.react("✅");
+								message.reply(`your tweet was approved by a user! Here it is: https://twitter.com/i/status/${tweet.id_str}`);
+							})
+							.catch(function (error) {
+								throw error;
+							});
+					} else if (reaction.emoji.name == "⬇") {
+						message.react("❌");
+						message.reply("your tweet was denied by a user.");
+					}
+				});
+		});
 	},
 };
