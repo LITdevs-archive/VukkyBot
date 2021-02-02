15399c7e6fdb8e4b501a4c8d499bc63e9e0c8f52
Vukky <46850780+Vukky123@users.noreply.github.com>
2021-02-02 09:09:43 +0100

    vukkyutils/localization

--- utilities/vukkytils.js
+++ utilities/vukkytils.js
@@ -1,6 +1,6 @@
 // Copyright (C) 2020-2021 Vukky
 
-const vukkytils = require("vukkyutils");
+const vukkytils = require("@vukkyutils/localization");
 const config = require("../config.json");
 
 vukkytils.setLanguage(config.misc.language);
