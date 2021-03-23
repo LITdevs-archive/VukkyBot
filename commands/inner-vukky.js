2e80a33dece88f45122cf56cd567614a5decde80
Vukky <46850780+Vukky123@users.noreply.github.com>
2021-03-23 21:51:21 +0100

    update date, more epic moments

--- commands/inner-vukky.js
+++ commands/inner-vukky.js
@@ -14,15 +14,27 @@ module.exports = {
 		let mode;
 		try {
 			let vukkyid;
-			if(message.mentions.users.first() && message.mentions.users.first().id !== message.author.id) {
-				vukkyid = vn.convert(message.mentions.users.first().id);
-				mode = "mentioned";
-				// message.mentions.users.first().username
-				message.channel.send(`${config.misc.emoji.loading} ${format(vukkytils.getString("DISCOVERING_OTHER_VUKKY"), message.mentions.users.first().username)}`)
+			// eslint-disable-next-line no-inner-declarations
+			function self() {
+				mode = "self";
+				vukkyid = vn.convert(message.author.id);
+				message.channel.send(`${config.misc.emoji.loading} ${vukkytils.getString("DISCOVERING_VUKKY")}`)
 					.then(newMessage => {	
-						newMessage.edit(format(vukkytils.getString("DISCOVERED_OTHER_VUKKY"), message.author, message.mentions.users.first().username), embeds.innerEmbed(`https://sivusto.tk/innervukky/${vukkyid}.png`));
+						newMessage.edit(format(vukkytils.getString("DISCOVERED_VUKKY"), message.author), embeds.innerEmbed(`https://sivusto.tk/innervukky/${vukkyid}.png`));
 					});
-			} else if (args[0]) { 
+			}
+			if(message.mentions.users.first()) {
+				if(message.mentions.users.first().id == message.author.id) {
+					self();
+				} else {
+					vukkyid = vn.convert(message.mentions.users.first().id);
+					mode = "mentioned";
+					message.channel.send(`${config.misc.emoji.loading} ${format(vukkytils.getString("DISCOVERING_OTHER_VUKKY"), message.mentions.users.first().username)}`)
+						.then(newMessage => {	
+							newMessage.edit(format(vukkytils.getString("DISCOVERED_OTHER_VUKKY"), message.author, message.mentions.users.first().username), embeds.innerEmbed(`https://sivusto.tk/innervukky/${vukkyid}.png`));
+						});
+				}
+			} else if (args[0] && !message.mentions.users.first()) { 
 				mode = "id";
 				await message.client.users.fetch(args[0]).then(function (res) {
 					vukkyid = vn.convert(res.id);
@@ -32,18 +44,13 @@ module.exports = {
 						});
 				});
 			} else {
-				mode = "self";
-				vukkyid = vn.convert(message.author.id);
-				message.channel.send(`${config.misc.emoji.loading} ${vukkytils.getString("DISCOVERING_VUKKY")}`)
-					.then(newMessage => {	
-						newMessage.edit(format(vukkytils.getString("DISCOVERED_VUKKY"), message.author), embeds.innerEmbed(`https://sivusto.tk/innervukky/${vukkyid}.png`));
-					});
+				self();
 			}
 		} catch (e) {
 			if(mode == "id") {
-				message.channel.send(`${config.misc.emoji.error} The Vukky seems to be hiding... Are you sure that's a real person?`);
+				message.channel.send(`${config.misc.emoji.error} ${vukkytils.getString("INNER_VUKKY_USER_FETCH_ERROR")}`);
 			} else {
-				message.channel.send(`${config.misc.emoji.error} The Vukky seems to be hiding...`);
+				message.channel.send(`${config.misc.emoji.error} ${vukkytils.getString("INNER_VUKKY_ERROR")}`);
 			}
 			console.error(e);
 		}
