c0b0be77fae6d31184ae1220ec689f9ae01473dd
Vukky <46850780+Vukky123@users.noreply.github.com>
2021-03-23 21:12:40 +0100

    Update inner-vukky.js

--- commands/inner-vukky.js
+++ commands/inner-vukky.js
@@ -10,33 +10,42 @@ module.exports = {
 	botPermissions: ["EMBED_LINKS"],
 	cooldown: 5,
 	aliases: ["innervukky"],
-	execute(message, args) {
-		let vukkyid;
-		let otherVukky;
-		if(message.mentions.users.first() && message.mentions.users.first().id !== message.author.id) {
-			vukkyid = vn.convert(message.mentions.users.first().id);
-			otherVukky = true;
-			// message.mentions.users.first().username
-			message.channel.send(`${config.misc.emoji.loading} ${format(vukkytils.getString("DISCOVERING_OTHER_VUKKY"), message.mentions.users.first().username)}`)
-				.then(newMessage => {	
-					newMessage.edit(format(vukkytils.getString("DISCOVERED_OTHER_VUKKY"), message.author, message.mentions.users.first().username), embeds.innerEmbed(`https://sivusto.tk/innervukky/${vukkyid}.png`));
-				});
-		} else if (args[0]) { 
-			message.client.users.fetch(args[0]).then(function (res) {
-				vukkyid = vn.convert(res.id);
-				otherVukky = true;
-				message.channel.send(`${config.misc.emoji.loading} ${format(vukkytils.getString("DISCOVERING_OTHER_VUKKY"), res.username)}`)
+	async execute(message, args) {
+		let mode;
+		try {
+			let vukkyid;
+			if(message.mentions.users.first() && message.mentions.users.first().id !== message.author.id) {
+				vukkyid = vn.convert(message.mentions.users.first().id);
+				mode = "mentioned";
+				// message.mentions.users.first().username
+				message.channel.send(`${config.misc.emoji.loading} ${format(vukkytils.getString("DISCOVERING_OTHER_VUKKY"), message.mentions.users.first().username)}`)
 					.then(newMessage => {	
-						newMessage.edit(format(vukkytils.getString("DISCOVERED_OTHER_VUKKY"), message.author, res.username), embeds.innerEmbed(`https://sivusto.tk/innervukky/${vukkyid}.png`));
+						newMessage.edit(format(vukkytils.getString("DISCOVERED_OTHER_VUKKY"), message.author, message.mentions.users.first().username), embeds.innerEmbed(`https://sivusto.tk/innervukky/${vukkyid}.png`));
 					});
-			});
-		} else {
-			vukkyid = vn.convert(message.author.id);
-			otherVukky = false;
-			message.channel.send(`${config.misc.emoji.loading} ${vukkytils.getString("DISCOVERING_VUKKY")}`)
-				.then(newMessage => {	
-					newMessage.edit(format(vukkytils.getString("DISCOVERED_VUKKY"), message.author), embeds.innerEmbed(`https://sivusto.tk/innervukky/${vukkyid}.png`));
+			} else if (args[0]) { 
+				mode = "id";
+				await message.client.users.fetch(args[0]).then(function (res) {
+					vukkyid = vn.convert(res.id);
+					message.channel.send(`${config.misc.emoji.loading} ${format(vukkytils.getString("DISCOVERING_OTHER_VUKKY"), res.username)}`)
+						.then(newMessage => {	
+							newMessage.edit(format(vukkytils.getString("DISCOVERED_OTHER_VUKKY"), message.author, res.username), embeds.innerEmbed(`https://sivusto.tk/innervukky/${vukkyid}.png`));
+						});
 				});
+			} else {
+				mode = "self";
+				vukkyid = vn.convert(message.author.id);
+				message.channel.send(`${config.misc.emoji.loading} ${vukkytils.getString("DISCOVERING_VUKKY")}`)
+					.then(newMessage => {	
+						newMessage.edit(format(vukkytils.getString("DISCOVERED_VUKKY"), message.author), embeds.innerEmbed(`https://sivusto.tk/innervukky/${vukkyid}.png`));
+					});
+			}
+		} catch (e) {
+			if(mode == "id") {
+				message.channel.send(`${config.misc.emoji.error} The Vukky seems to be hiding... Are you sure that's a real person?`);
+			} else {
+				message.channel.send(`${config.misc.emoji.error} The Vukky seems to be hiding...`);
+			}
+			console.error(e);
 		}
 	},
 };
